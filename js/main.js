c63550ab6b3e83a1c9dfa2323cf1e675df604ac5
FX Mobile 5 <mdanial@johnmcneilstudio.com>
2015-07-24 10:05:46 -0700

    responsive styling

--- js/main.js
+++ js/main.js
@@ -2,4 +2,9 @@ $(window).load(function() {
   $('.flexslider').flexslider({
   	slideshow: false
   });
+
+  $('.hamburger').on('click', function(){
+  	$('.header-social-wrapper').toggleClass('mobile-shown');
+  	$('.nav-header').toggleClass('mobile-shown');
+  });
 });
